56176559e63afcf7fae38ab4875d3a39669ac940
Programmoh <moh.elnaggar2020@gmail.com>
2025-08-02 15:40:30 -0600

    set up the express backend barebones

---
+++ quantsim-server/index.js
@@ -0,0 +1,20 @@
+// entry point for our Express.js backend (set up server, middleware, and routing)
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000; //we"ll change the port later on
+
+// communication middleware
+app.use(cors());
+app.use(express.json());
+
+// api routes basis
+const testRoutes = require('./routes/testRoutes');
+app.use('/api', testRoutes);
+
+// get the server live
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
